c14a360b5216871ac8fbe38bc1a4a078daa366cf
Arun CS <aruncs31ss@gmail.com>
2025-10-18 18:04:34 +0530

    added project module back

---
+++ project.go
@@ -0,0 +1,50 @@
+package model
+
+import (
+	"time"
+)
+
+type Project struct {
+	ID           int             `gorm:"primaryKey"`
+	Title        string          `gorm:"column:title;unique"`
+	Image        *string         `gorm:"column:image"`
+	Description  string          `gorm:"column:description"`
+	GithubLink   string          `gorm:"column:github_link"`
+	LiveURL      *string         `gorm:"column:live_url"`
+	Likes        int             `gorm:"column:likes;default:0"`
+	Category     string          `gorm:"column:category;default:'General'"`
+	Cost         int             `gorm:"column:cost;default:0"`
+	Status       string          `gorm:"column:status;default:'active'"` // active, inactive, archived
+	Visibility   int             `gorm:"column:visibility;default:0"`    // 0: public, 1: private
+	CreatedBy    uint            `gorm:"column:created_by;not null"`
+	ModifiedBy   *uint           `gorm:"column:modified_by"`
+	CreatedAt    *time.Time      `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt    *time.Time      `gorm:"column:updated_at;autoUpdateTime"`
+	Creator      User            `gorm:"foreignKey:CreatedBy;references:ID" json:"creator"`
+	Contributors *[]User         `gorm:"many2many:project_contributors;" json:"contributors"`
+	Tags         *[]Tag          `gorm:"many2many:project_tags;" json:"tags"`
+	Technologies *[]Technologies `gorm:"many2many:project_technologies;" json:"technologies"`
+	LikedBy      []User          `gorm:"many2many:project_likes;" json:"liked_by"`
+}
+
+func (Project) TableName() string {
+	return "projects"
+}
+
+type Tag struct {
+	ID   uint   `gorm:"primaryKey;autoIncrement"`
+	Name string `gorm:"column:name;unique;not null"`
+}
+
+func (Tag) TableName() string {
+	return "tags"
+}
+
+type Technologies struct {
+	ID   int    `gorm:"column:id;primaryKey;autoIncrement"`
+	Name string `gorm:"column:name;unique;not null"`
+}
+
+func (Technologies) TableName() string {
+	return "technologies"
+}
