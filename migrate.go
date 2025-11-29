dc9749fa5a3f786c398e1c11836b9af6e858df4f
Arun cs <aruncs31ss@gmail.com>
2025-11-30 03:02:07 +0530

    added migrate also

---
+++ migrate.go
@@ -0,0 +1,39 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+// Migrate runs auto-migration for all models in the package.
+// It creates tables, missing foreign keys, constraints, columns and indexes.
+func Migrate(db *gorm.DB) error {
+	return db.AutoMigrate(
+		// Core models
+		&User{},
+		&Project{},
+		&Tag{},
+		&Technologies{},
+		&Comments{},
+		&Reviews{},
+		&Order{},
+		&Post{},
+		&Teams{},
+		&Submission{},
+		&Notification{},
+		&ProjectTemplate{},
+		&ProjectStats{},
+		&ProjectAnalytics{},
+
+		// Trending models
+		&TrendingTech{},
+		&TrendingTag{},
+		&TrendingCategory{},
+		&TrendingAuthor{},
+		&TrendingSource{},
+
+		// Integration models
+		&ChatBotMessage{},
+		&Github{},
+		&Ollama{},
+	)
+}
