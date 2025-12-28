99c81a52ee34d8722e7b77fcc91b672a2a8d74c2
Arun CS <arun.cs@etlab.in>
2025-12-28 18:09:18 -0500

    new model course

--- course.go
+++ course.go
@@ -1,6 +1,6 @@
 package model
 
-type Corse struct {
+type Course struct {
 	ID          uint   `gorm:"primaryKey;autoIncrement" json:"id"`
 	Title       string `gorm:"type:varchar(100);not null" json:"title"`
 	Description string `gorm:"type:text" json:"description"`
@@ -9,6 +9,6 @@ type Corse struct {
 	Enrollments uint   `gorm:"default:0" json:"enrollments"`
 }
 
-func (Corse) TableName() string {
+func (Course) TableName() string {
 	return "courses"
 }
